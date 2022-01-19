a4987c76f0a0a6f292c8ae20b63a42bdcec18f76
takuminishi0930 <takuminishi0930@icloud.com>
2022-01-19 20:15:07 +0900

    modified

--- setup.sh
+++ setup.sh
@@ -1,5 +1,51 @@
-sudo apt-get update
-sudo apt-get upgrade
-cd ダウンロード
-sudo apt install ./google-chrome-stable_current_amd64.deb
-sudo apt install ./code_1.62.0-1635954068_amd64.deb
+cd
+
+#sudo apt install git -y
+mkdir program
+cd program
+git clone https://github.com/takuminishi0930/migration
+git clone https://github.com/takuminishi0930/gyoshock-battle
+git clone https://github.com/takuminishi0930/gyoshock-karuta
+git clone https://github.com/takuminishi0930/betaTwiGather
+git clone https://github.com/takuminishi0930/TwiGather
+git clone https://github.com/takuminishi0930/indicators
+sudo snap install --classic heroku
+heroku login
+
+LANG=C xdg-user-dirs-gtk-update
+sudo apt install fcitx-mozc
+
+cd
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt install ./google-chrome-stable_current_amd64.debsudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo rm microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt install apt-transport-https
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt-get install google-chrome-stable
+sudo apt install code
+sudo apt install python3-pip -y
+sudo apt-get install -y make
+sudo apt-get install -y curl
+sudo apt-get install -y file
+sudo apt-get install -y libmecab-dev
+sudo apt-get install -y mecab
+sudo apt-get install -y mecab-ipadic-utf8
+sudo apt-get install -y gcc
+sudo apt-get install -y pip
+git clone --depth 1 https://github.com/neologd/mecab-ipadic-neologd.git
+mecab-ipadic-neologd/bin/install-mecab-ipadic-neologd -y
+cd
+cd program/migration
+pip3 install -r requirements.txt
+
+cd
+mkdir ~/.ssh
+cd ~/.ssh
+ssh-keygen -t rsa
+sudo apt install xsel
+cat id_rsa.pub | xsel --clipboard --input
+#githubで秘密鍵を登録する　https://github.com/settings/ssh
---
+++ setup2.sh
@@ -0,0 +1,14 @@
+cd /home/takumi/program/migration
+git remote set-url origin git@github.com:takuminishi0930/migration.git
+cd /home/takumi/program/gyoshock-battle
+git remote set-url origin git@github.com:takuminishi0930/gyoshock-battle.git
+cd /home/takumi/program/gyoshock-karuta
+git remote set-url origin git@github.com:takuminishi0930/gyoshock-karuta.git
+cd /home/takumi/program/betaTwiGather
+git remote set-url origin git@github.com:takuminishi0930/betaTwiGather.git
+git remote add heroku https://git.heroku.com/TwiGather.git
+cd /home/takumi/program/TwiGather
+git remote set-url origin git@github.com:takuminishi0930/TwiGather.git
+cd /home/takumi/program/indicators
+git remote set-url origin git@github.com:takuminishi0930/indicators.git
+git remote add heroku https://git.heroku.com/keyindicators.git
