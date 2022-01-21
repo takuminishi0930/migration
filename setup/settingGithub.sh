eca82d3fa538f4ab173e63d73e5ca6e1be23a91c
takuminishi0930 <takuminishi0930@icloud.com>
2022-01-22 01:27:22 +0900

    modified

--- setup/settingGithub.sh
+++ setup/settingGithub.sh
@@ -18,7 +18,7 @@ cd /home/takumi/program/gyoshock-karuta
 git remote set-url origin git@github.com:takuminishi0930/gyoshock-karuta.git
 cd /home/takumi/program/betaTwiGather
 git remote set-url origin git@github.com:takuminishi0930/betaTwiGather.git
-git remote add heroku https://git.heroku.com/TwiGather.git
+git remote add heroku https://git.heroku.com/twigather.git
 cd /home/takumi/program/TwiGather
 git remote set-url origin git@github.com:takuminishi0930/TwiGather.git
 cd /home/takumi/program/indicators
