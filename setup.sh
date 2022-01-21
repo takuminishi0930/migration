613afa9e7c3db0b0e00b94e064e73ca747821cb4
takuminishi0930 <takuminishi0930@icloud.com>
2022-01-21 23:33:21 +0900

    modified

--- setup.sh
+++
@@ -1,3 +0,0 @@
-LANG=C xdg-user-dirs-gtk-update
-sudo apt-get update
-sudo apt-get upgrade
--- setup/setup.sh
+++ setup/setup.sh
@@ -1,3 +1,10 @@
 LANG=C xdg-user-dirs-gtk-update
 sudo apt-get update
 sudo apt-get upgrade
+
+bash installChrome.sh
+bash installVscode.sh
+bash mecab.sh
+bash heroku.sh
+bash settingGithub.sh
+bash sshkey.sh
